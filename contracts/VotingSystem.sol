1432864d788d515056ef9ba9fc17c8a659f678f7
Erwin Wijaya <erwin_wijaya@apple.com>
2021-12-08 13:05:54 +0800

    Allow anyone to setup a voting system

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -6,6 +6,7 @@ contract VotingSystem {
         string name;
         uint voteCounts;
         address[] voters;
+        uint votersCounts;
     }
 
     struct Voter {
@@ -19,12 +20,13 @@ contract VotingSystem {
     mapping (address => Voter) public voters;
 
     address rewarded_address;
+    address[] voted_address;
     bool voting_period_ended = false;
+    uint num_of_choices = 0;
 
     modifier allowedToVote
     {
         require (block.timestamp >= startTime, "Voting period has not started yet");
-        rewardRandomVoterFromHighestVote();
         require (block.timestamp < endTime, "Voting period has ended");
         require (voters[msg.sender].voted == false, "Voter already used his/her right to vote.");
         _;
@@ -37,8 +39,40 @@ contract VotingSystem {
             Choice memory init;
             init.name = _choices[idx];
             init.voteCounts = 0;
+            init.votersCounts = 0;
             choices.push(init);
+            num_of_choices++;
         }
+        startTime = block.timestamp + _timeBeforeVotingStartInSecs;
+        endTime = startTime + _votingDurationInSecs;
+        voting_period_ended = false;
+        rewarded_address = address(0);
+    }
+
+    function createNewElection (string[] memory _choices, uint256 _timeBeforeVotingStartInSecs, uint256 _votingDurationInSecs) public payable
+    {
+        require(msg.value > 0.1 ether, "required to send > 0.1 ether to reward a random voter"); // to reward a random voter after voting period has ended
+        require (block.timestamp > endTime, "Voting period has NOT ended");
+        rewardRandomVoterFromHighestVote();
+        num_of_choices = 0;
+        
+        for (uint idx = 0; idx < _choices.length; idx++)
+        {
+            Choice memory init;
+            init.name = _choices[idx];
+            init.voteCounts = 0;
+            init.votersCounts = 0;
+            if (idx < choices.length)
+            {
+                choices[idx] = init;
+            }
+            else
+            {
+                choices.push(init);
+            }
+            num_of_choices++;
+        }
+        
         startTime = block.timestamp + _timeBeforeVotingStartInSecs;
         endTime = startTime + _votingDurationInSecs;
         voting_period_ended = false;
@@ -47,15 +81,27 @@ contract VotingSystem {
 
     function vote (uint _choicesIdx) public allowedToVote
     {
-        require(_choicesIdx >= 0 && _choicesIdx < choices.length, "vote is outside the range of available choices");
+        require(_choicesIdx >= 0 && _choicesIdx < num_of_choices, "vote is outside the range of available choices");
         voters[msg.sender].voted = true;
         choices[_choicesIdx].voteCounts += 1;
-        choices[_choicesIdx].voters.push(msg.sender);
+        
+        if (choices[_choicesIdx].votersCounts < choices[_choicesIdx].voters.length)
+        {
+            //re-use the same allocated memory for storing voters address
+            choices[_choicesIdx].voters[choices[_choicesIdx].votersCounts] = msg.sender;
+        }
+        else
+        {
+            choices[_choicesIdx].voters.push(msg.sender);
+        }
+
+        choices[_choicesIdx].votersCounts += 1;
+        voted_address.push(msg.sender);
     }
 
     function getNumberOfVote (uint _choicesIdx) public view returns (uint)
     {
-        require(_choicesIdx >= 0 && _choicesIdx < choices.length, "vote is outside the range of available choices");
+        require(_choicesIdx >= 0 && _choicesIdx < num_of_choices, "vote is outside the range of available choices");
         uint counts = choices[_choicesIdx].voteCounts;
         return counts;
     }
@@ -75,7 +121,7 @@ contract VotingSystem {
         { 
             voting_period_ended = true;
             uint winningChoice = findHighestVoteIdx();
-            uint randomVoterIdx = block.timestamp % choices[winningChoice].voters.length; 
+            uint randomVoterIdx = block.timestamp % choices[winningChoice].votersCounts; 
             rewarded_address = choices[winningChoice].voters[randomVoterIdx];
             (bool success, ) = rewarded_address.call{value:0.1 ether}("");
             require( success, "Reward 0.1 ETH Failed");
@@ -85,7 +131,7 @@ contract VotingSystem {
     function findHighestVoteIdx() public view returns (uint)
     {
         uint highestVoteIdx = 0;
-        for (uint idx = 1; idx < choices.length; idx++) {
+        for (uint idx = 1; idx < num_of_choices; idx++) {
             if (choices[idx].voteCounts > choices[highestVoteIdx].voteCounts) 
             {
                 highestVoteIdx = idx;
--- migrations/2_VotingSystem_migration.js
+++ migrations/2_VotingSystem_migration.js
@@ -7,20 +7,23 @@ module.exports = function (deployer) {
   deployer.deploy(VotingSystem,["Erwin", "Elsit", "Chres"], 15, 45);
 
   /* Execute the following commands in the console log to test
+  v = await VotingSystem.deployed();
+  v.deposit({value:web3.utils.toWei("0.2","ether")})
+  v.vote(0, {from: accounts[0]});
+  v.vote(0, {from: accounts[1]});
+  v.vote(1, {from: accounts[2]});
+  v.vote(2, {from: accounts[3]});
 
-  votingSystemInstance = await VotingSystem.deployed();
-  votingSystemInstance.deposit({value:web3.utils.toWei("0.2","ether")})
-  votingSystemInstance.vote(0, {from: accounts[0]});
-  votingSystemInstance.vote(0, {from: accounts[1]});
-  votingSystemInstance.vote(1, {from: accounts[2]});
-  votingSystemInstance.vote(2, {from: accounts[3]});
-
-  votingSystemInstance.getNumberOfVote(0).then(function(v) {c1=v})
-  votingSystemInstance.getNumberOfVote(1).then(function(v) {c2=v})
-  votingSystemInstance.getNumberOfVote(2).then(function(v) {c3=v})
+  v.getNumberOfVote(0).then(function(w) {c1=w})
+  v.getNumberOfVote(1).then(function(w) {c2=w})
+  v.getNumberOfVote(2).then(function(w) {c3=w})
 
   vote1 = c1.words[0]
   vote2 = c2.words[0]
   vote3 = c3.words[0]
+
+  web3.eth.getBalance(accounts[0])
+
+  v.createNewElection(["Erwin", "Elsit", "Chres"], 15, 45, {value: web3.utils.toWei('0.11', 'ether')})
   */
 };
